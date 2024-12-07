27bd5772af4ddc22578f4affaf1856140a30c0eb
Abhishek2010DevSingh <162547826+Abhishek2010DevSingh@users.noreply.github.com>
2024-12-07 16:16:33 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,12 +1,3 @@
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
-vim.o.expandtab = true
-vim.o.smartindent = true
-vim.o.clipboard = "unnamedplus"
-vim.cmd("filetype plugin indent on")
-
 require("config.lazy")
 require("config.lsp")
 require("config.basic_config")
